363714967738414c061d813367d227e85136c370
Krystian Grzelak <krystian.grzelak@o2.pl>
2023-10-10 16:17:43 +0200

    feat: fix tests

--- src/app/backend/users/users.service.spec.ts
+++ src/app/backend/users/users.service.spec.ts
@@ -1,12 +1,17 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { API_URL } from '@providers';
 
 import { UsersService } from './users.service';
 
-describe('UsersService', () => {
+describe(UsersService.name, () => {
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: API_URL, useValue: '' }],
+    });
     service = TestBed.inject(UsersService);
   });
 
--- src/app/views/main/views/users/candidates/candidates.component.spec.ts
+++ src/app/views/main/views/users/candidates/candidates.component.spec.ts
@@ -1,14 +1,21 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { API_URL } from '@providers';
+import { MessageService } from 'primeng/api';
 
 import { CandidatesComponent } from './candidates.component';
 
-describe('CandidatesComponent', () => {
+describe(CandidatesComponent.name, () => {
   let component: CandidatesComponent;
   let fixture: ComponentFixture<CandidatesComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CandidatesComponent],
+      imports: [CandidatesComponent, HttpClientTestingModule, TranslateModule.forRoot()],
+      providers: [{ provide: API_URL, useValue: '' }, MessageService],
+      schemas: [NO_ERRORS_SCHEMA],
     });
     fixture = TestBed.createComponent(CandidatesComponent);
     component = fixture.componentInstance;
