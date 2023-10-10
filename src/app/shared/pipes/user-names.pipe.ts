9928f964f8016a0f72182aa5154a9b09f560e7fa
Krystian Grzelak <krystian.grzelak@o2.pl>
2023-10-10 11:23:42 +0200

    feat: update usernames pipe

--- src/app/shared/pipes/user-names.pipe.ts
+++ src/app/shared/pipes/user-names.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { UserInterface } from '@backend/users';
+import { RoleEnum } from '@shared/enums';
 
 @Pipe({
   name: 'userNames',
@@ -7,6 +8,8 @@ import { UserInterface } from '@backend/users';
 })
 export class UserNamesPipe implements PipeTransform {
   public transform(user: UserInterface): string {
-    return `${user.firstName} ${user.lastName}`;
+    if (user.role === RoleEnum.USER) return `${user.firstName} ${user.lastName}`;
+
+    return `(${user.role}) ${user.firstName} ${user.lastName}`;
   }
 }
